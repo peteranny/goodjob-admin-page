27ade3f67db7dda852767d4eef54a795300e01e8
Wendell Liu <cuk.bas@gmail.com>
2018-06-27 17:25:54 +0800

    Docker setup (#15)
    pre-build --> build --> server
    1. express static serve build folder
    2. express serve all path to /build/index.html

---
+++ server.js
@@ -0,0 +1,18 @@
+// @flow
+const express = require('express');
+const path = require('path');
+const {
+  appBuild,
+} = require('./config/paths');
+
+
+const app = express();
+const PORT = process.env.PORT || 80;
+
+app.use(express.static(appBuild));
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(appBuild, '/index.html')),
+);
+
+app.listen(PORT, () => console.log(`the app listening on ${PORT}!`));
