3e06da6cfbcc11518ce81630685f420c3b2c051a
Yen-chi Chen <mark86092@gmail.com>
2018-09-06 14:01:07 +0800

    Make eslint say error if not meet prettier standard (#30)
    * Make eslint say error if not meet prettier standard
    
    * Run `npm run lint -- --fix`

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 // @flow
 const express = require('express');
 const path = require('path');
-const {
-  appBuild,
-} = require('./config/paths');
-
+const { appBuild } = require('./config/paths');
 
 const app = express();
 const PORT = process.env.PORT || 80;
 
 app.use(express.static(appBuild));
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(appBuild, '/index.html')),
-);
+app.get('*', (req, res) => res.sendFile(path.join(appBuild, '/index.html')));
 
 app.listen(PORT, () => console.log(`the app listening on ${PORT}!`));
