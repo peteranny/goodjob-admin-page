f6765882f4dfe6c65ce2bbf63f93df33044e6fc3
Wendell Liu <cuk.bas@gmail.com>
2018-11-18 18:22:00 +0800

    add flow

--- src/shared/hoc/withHandleTableChange.js
+++ src/shared/hoc/withHandleTableChange.js
@@ -1,5 +1,32 @@
+// @flow
 import { withHandlers, type HOC } from 'recompose';
 import { isEmpty } from 'ramda';
+
+import { type OrderBy } from '../constants';
+import type { ExperienceType } from '../types/experienceType';
+
+type Props = {
+  setPage: number => void,
+  setSortObj: ({
+    sortField: string,
+    orderBy: OrderBy
+  }) => void,
+  page: number,
+  pageSize: number,
+  searchObj: {
+    columnKey: string,
+    value: string
+  },
+  sortObj: {
+    sortField: string,
+    orderBy: OrderBy
+  },
+  setSearchObj: ({
+    columnKey: string,
+    value: string
+  }) => void,
+  expData: Array<ExperienceType>
+};
 
 const withHandleTableChange: HOC<*, Props> = withHandlers({
   handleTableChange: ({ setPage, setSortObj }) => (nextPagination, _, nextSorter) => {
